903881ad7a3b305c2688046d8a9347cd7d8027d9
Rasbats <miker@members.swis.net>
2021-12-29 10:15:19 +0000

    Templates: Updating cmake

--- cmake/PluginLocalization.cmake
+++ cmake/PluginLocalization.cmake
@@ -1,30 +1,40 @@
 # ~~~
-# Author:      Pavel Kalian / Sean D'Epagnier
-# Copyright:
-# License:     GPLv3+
-#
-# Purpose:     Generate and install translations
+# Summary:      Generate and install translations
+# Author:       Pavel Kalian / Sean D'Epagnier
+# Copyright (c) 2014 Pavel Kallian
+# License:      GPLv3+
 # ~~~
 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+
+
 find_program(GETTEXT_XGETTEXT_EXECUTABLE xgettext)
+
+string(REPLACE "_pi" "" I18N_NAME ${PACKAGE_NAME})
+
 if (GETTEXT_XGETTEXT_EXECUTABLE)
-  if (NOT TARGET pot-update)
-    add_custom_target(pot-update COMMENT "Created pot file")
-  endif ()
   add_custom_command(
-    TARGET pot-update
+    OUTPUT po/${PACKAGE_NAME}.pot.dummy
     COMMAND
       ${GETTEXT_XGETTEXT_EXECUTABLE} --force-po --package-name=${PACKAGE_NAME}
       --package-version="${PROJECT_VERSION}" --output=po/${PACKAGE_NAME}.pot
       --keyword=_ --width=80
-      --files-from=${CMAKE_BINARY_DIR}/POTFILES.in
+      --files-from=${CMAKE_CURRENT_SOURCE_DIR}/po/POTFILES.in
     DEPENDS po/POTFILES.in po/${PACKAGE_NAME}.pot
     WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-    COMMENT "pot-update: Generated pot file po/${PACKAGE_NAME}.pot."
+    COMMENT "${I18N_NAME}-pot-update [${PACKAGE_NAME}]: Generated pot file."
+  )
+  add_custom_target(
+    ${I18N_NAME}-pot-update
+    COMMENT "[${PACKAGE_NAME}]-pot-update: Done."
+    DEPENDS po/${PACKAGE_NAME}.pot.dummy
   )
 endif (GETTEXT_XGETTEXT_EXECUTABLE)
 
-macro (gettext_update_po _potFile)
+macro (GETTEXT_UPDATE_PO _potFile)
   set(_poFiles ${_potFile})
   get_filename_component(_absPotFile ${_potFile} ABSOLUTE)
 
@@ -37,18 +47,17 @@ macro (gettext_update_po _potFile)
       COMMAND ${GETTEXT_MSGMERGE_EXECUTABLE} --width=80 --strict --quiet
               --update --backup=none --no-location -s ${_absFile} ${_absPotFile}
       DEPENDS ${_absPotFile} ${_absFile}
-      COMMENT "po-update [${_poBasename}]: Updated po file."
+      COMMENT "${I18N_NAME}-po-update [${_poBasename}]: Updated po file."
     )
     set(_poFiles ${_poFiles} ${_absFile}.dummy)
   endforeach (_currentPoFile)
-  if (NOT TARGET po-update)
-    add_custom_target(
-      po-update
-      COMMENT "[${PACKAGE_NAME}]-po-update: Done."
-      DEPENDS ${_poFiles}
-    )
-  endif ()
-endmacro (gettext_update_po)
+
+  add_custom_target(
+    ${I18N_NAME}-po-update
+    COMMENT "[${PACKAGE_NAME}]-po-update: Done."
+    DEPENDS ${_poFiles}
+  )
+endmacro (GETTEXT_UPDATE_PO)
 
 if (GETTEXT_MSGMERGE_EXECUTABLE)
   file(GLOB PACKAGE_PO_FILES po/*.po)
@@ -56,7 +65,7 @@ if (GETTEXT_MSGMERGE_EXECUTABLE)
 endif (GETTEXT_MSGMERGE_EXECUTABLE)
 
 set(_gmoFiles)
-macro (gettext_build_mo)
+macro (GETTEXT_BUILD_MO)
   foreach (_poFile ${ARGN})
     get_filename_component(_absFile ${_poFile} ABSOLUTE)
     get_filename_component(_poBasename ${_absFile} NAME_WE)
@@ -68,7 +77,7 @@ macro (gettext_build_mo)
       COMMAND ${CMAKE_COMMAND} -E copy ${_gmoFile}
               "Resources/${_poBasename}.lproj/opencpn-${PACKAGE_NAME}.mo"
       DEPENDS ${_absFile}
-      COMMENT "i18n [${_poBasename}]: Created mo file."
+      COMMENT "${I18N_NAME}-i18n [${_poBasename}]: Created mo file."
     )
     if (APPLE)
       install(
@@ -86,17 +95,16 @@ macro (gettext_build_mo)
 
     set(_gmoFiles ${_gmoFiles} ${_gmoFile})
   endforeach (_poFile)
-endmacro (gettext_build_mo)
+endmacro (GETTEXT_BUILD_MO)
 
 if (GETTEXT_MSGFMT_EXECUTABLE)
   file(GLOB PACKAGE_PO_FILES po/*.po)
   gettext_build_mo(${PACKAGE_PO_FILES})
-  if (NOT TARGET i18n)
-    add_custom_target(
-      i18n
-      COMMENT "${PACKAGE_NAME}-i18n: Done."
-      DEPENDS ${_gmoFiles}
-    )
-  endif ()
-  add_dependencies(${PACKAGE_NAME} i18n)
+  add_custom_target(
+    ${I18N_NAME}-i18n
+    COMMENT "${PACKAGE_NAME}-i18n: Done."
+    DEPENDS ${_gmoFiles}
+  )
+  add_dependencies(${PACKAGE_NAME} ${I18N_NAME}-i18n)
+  add_dependencies( tarball ${I18N_NAME}-i18n)
 endif (GETTEXT_MSGFMT_EXECUTABLE)
